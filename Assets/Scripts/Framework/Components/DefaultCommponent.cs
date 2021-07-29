15dd5c68eaac8f4257debdcd03c35cefa663d3b7
kaclok <kaclok@users.noreply.github.com>
2021-07-29 21:00:13 +0800

    脚本

--- Assets/Scripts/Framework/Components/DefaultCommponent.cs
+++ Assets/Scripts/Framework/Components/DefaultCommponent.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
 using UnityEngine;
 
 [DisallowMultipleComponent]
-public class DefaultCommponent<T> where T : Component {
-    public T defaultValue;
-    public T currentValue;
+public class DefaultCommponent<T> : MonoBehaviour where T : Component {
+    public static T defaultValue { get; private set; } = null;
+    public static T finalValue { get; private set; } = null;
+    
+    private T _self = null;
 
-    public Action<T, T> onChanged;
+    public T self {
+        get {
+            if (_self == null) {
+                _self = GetComponent<T>();
+            }
 
+            return _self;
+        }
+    }
+    
+    private void OnEnable() {
+        if (finalValue != null) {
+            finalValue = self;
+        }
+        else {
+            defaultValue = finalValue = self;
+        }
+    }
+
+    private void OnDisable() {
+        if (finalValue != null) {
+            finalValue = self;
+        }
+    }
 }
