b2ae3877a9474b4009954353a651535dbcf8bc63
kaclok <kaclok@users.noreply.github.com>
2021-07-31 15:33:07 +0800

    文件转移位置

--- Assets/Scripts/Framework/UI/UIBaseT.cs
+++ Assets/Scripts/Framework/UI/UIBaseT.cs
@@ -81,22 +81,3 @@ public class UIBaseT {
 
     #endregion
 }
-
-[Serializable]
-public class UIBase<TLayout> : UIBaseT where TLayout : UILayoutBase, new() {
-    [SerializeField] public TLayout layout;
-
-    private void Load() {
-        // 加载prefab资源
-        Transform instance = null;
-
-        this.layout = new TLayout();
-        this.layout.TryInit(instance);
-
-        OnLoaded();
-    }
-
-    private void OnLoaded() {
-        OnInit();
-    }
-}
---
+++ Assets/Scripts/Runtime/Hotfix/UI/UIBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class UIBase<TLayout> : UIBaseT where TLayout : UILayoutBase, new() {
+    [SerializeField] public TLayout layout;
+
+    private void Load() {
+        // 加载prefab资源
+        Transform instance = null;
+
+        this.layout = new TLayout();
+        this.layout.TryInit(instance);
+
+        OnLoaded();
+    }
+
+    private void OnLoaded() {
+        OnInit();
+    }
+}
