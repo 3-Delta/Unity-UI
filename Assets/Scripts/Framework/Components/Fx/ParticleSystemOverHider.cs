939a74b25bc80548c00d05c49f778174f18962a9
cuibinbin <18729485430@139.com>
2021-10-28 20:58:16 +0800

    特效组件

---
+++ Assets/Scripts/Framework/Components/Fx/ParticleSystemOverHider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+// 给非循环类型的fx挂载
+[DisallowMultipleComponent]
+public class AutoHider : MonoBehaviour {
+    public enum EAutoOp {
+        Hide,
+        Destroy,
+    }
+
+    public EAutoOp op = EAutoOp.Hide;
+    public bool useFxDuration = true;
+    [Range(0.02f, 10f)] public float duration = 0.02f;
+
+    private float maxDuration;
+
+    public const string FUNCNAME = "Auto";
+
+    private void Start() {
+        if (useFxDuration) {
+            var ls = GetComponentsInChildren<ParticleSystem>();
+            maxDuration = 0f;
+            foreach (var one in ls) {
+                if (one.main.duration > maxDuration) {
+                    maxDuration = one.main.duration;
+                }
+            }
+
+            if (maxDuration <= 0.02f) {
+                maxDuration = 0.02f;
+            }
+        }
+    }
+
+    private void OnEnable() {
+        CancelInvoke(FUNCNAME);
+        Invoke(FUNCNAME, useFxDuration ? maxDuration : duration);
+    }
+
+    private void Auto() {
+        if (op == EAutoOp.Hide) {
+            gameObject.SetActive(false);
+        }
+        else if (op == EAutoOp.Destroy) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/Components/Fx/ParticleSystemSetter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ParticleSystemSetter : MonoBehaviour {
+    [SerializeField] private ParticleSystem[] fxList;
+
+    private void Start() {
+        fxList = GetComponentsInChildren<ParticleSystem>();
+
+        foreach (var fx in fxList) {
+            // fx.main.scalingMode = ParticleSystemScalingMode.Hierarchy;
+            // fx.scalingMode = ParticleSystemScalingMode.Hierarchy;
+        }
+    }
+}
