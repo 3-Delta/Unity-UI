522143b257c50ce8ada04465cc135c7d3bc246ee
cuibinbin <18729485430@139.com>
2021-11-04 11:32:33 +0800

    TPacker
    Unity中解决JsonUtility转换数组失败的BUG

---
+++ Assets/Scripts/Framework/Utility/TPacker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TPacker<T> {
+    public T value;
+}
+
+// https://segmentfault.com/a/1190000015291516
+// Unity中解决JsonUtility转换数组失败的BUG
+public class JsonUtilityArrayPacker {
+    public static string ToJson<T>(T target) {
+        if (typeof(T).GetInterface("IList") != null) {
+            TPacker<T> pack = new TPacker<T>();
+            pack.value = target;
+            string json = JsonUtility.ToJson(pack);
+            return json.Substring(8, json.Length - 9);
+        }
+
+        return JsonUtility.ToJson(target);
+    }
+
+    public static T FromJson<T>(string json) {
+        if (typeof(T).GetInterface("IList") != null) {
+            json = "{\"value\":{value}}".Replace("{value}", json);
+            TPacker<T> Pack = JsonUtility.FromJson<TPacker<T>>(json);
+            return Pack.value;
+        }
+
+        return JsonUtility.FromJson<T>(json);
+    }
+}
