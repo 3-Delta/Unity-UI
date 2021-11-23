1a7dc6e4a5b25938cfb396f4b058b3ab6ce832d3
cuibinbin <18729485430@139.com>
2021-11-23 13:05:58 +0800

    StringUtility.cs

---
+++ Assets/Scripts/Framework/Utility/StringUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+public static class StringUtility {
+    public static bool IsChineseLetter(char ch) {
+        Regex reg = new Regex(@"[\u4e00-\u9fa5]");
+        return reg.IsMatch(ch.ToString());
+    }
+
+    public static int ChineseLetterCount(string input) {
+        Regex reg = new Regex(@"[\u4e00-\u9fa5]");
+        return reg.Matches(input).Count;
+    }
+
+    public static bool HasChineseLetter(string input) {
+        return ChineseLetterCount(input) > 0;
+    }
+}
