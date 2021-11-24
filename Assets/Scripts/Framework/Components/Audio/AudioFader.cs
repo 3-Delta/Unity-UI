5dd0ac3fd65fe8a05aaf7c87dc3138afcc342966
cuibinbin <18729485430@139.com>
2021-11-24 17:21:56 +0800

    IFader 淡入淡出组价

---
+++ Assets/Scripts/Framework/Components/Audio/AudioFader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public interface IFader<T> {
+    void Fade(T t);
+
+    void OnBeginFade();
+    void OnFading(double timeSinceBegin);
+    void OnEndFade();
+}
+
+[DisallowMultipleComponent]
+public class AudioFader : MonoBehaviour, IFader<AudioClip> {
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    private IFader<AudioClip> _faderImplementation;
+
+    private void Awake() {
+        if (audioSource != null) {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public void SetAudioSource(AudioSource target) {
+        audioSource = target;
+    }
+
+    public void Fade(AudioClip audioClip) {
+        if (audioSource == null) {
+            return;
+        }
+
+        this.audioClip = audioClip;
+        if (audioSource.clip == null) {
+            // 直接播放，不fade
+            OnBeginFade();
+            OnEndFade();
+        }
+        else {
+            if (audioClip == audioSource.clip) {
+                // do nothing
+            }
+            else {
+                // 借助timer实现fading
+            }
+        }
+    }
+
+    public void OnBeginFade() { }
+
+    public void OnFading(double timeSinceBegin) { }
+
+    public void OnEndFade() { }
+}
