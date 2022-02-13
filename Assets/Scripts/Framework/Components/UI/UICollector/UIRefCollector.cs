c16a0bf82e12692f4da765c1d9736db372192ede
cignal <cignal@users.noreply.github.com>
2022-02-13 17:56:11 +0800

    实现UIRefCollector,简化UI工作中的组件代码查找 缺少： 1. 拖入组件的时候，自动生成对应的名字 2. 拖入组件的时候， 有点儿不方便 https://github.com/XINCGer/UnityToolchainsTrick https://github.com/CatImmortal/ComponentAutoBindTool

---
+++ Assets/Scripts/Framework/Components/UI/UICollector/UIRefCollector.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System.Text;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomPropertyDrawer(typeof(UIRefCollector.BindItem))]
+public class BindItemDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        using (new EditorGUI.PropertyScope(position, label, property)) {
+            EditorGUIUtility.labelWidth = 120;
+            position.height = EditorGUIUtility.singleLineHeight;
+
+            Rect componentRect = new Rect(position) {
+                x = position.x + 30,
+                width = 130
+            };
+            var component = property.FindPropertyRelative("component");
+            EditorGUI.PropertyField(componentRect, component, GUIContent.none);
+
+            Rect nameRect = new Rect(componentRect) {
+                x = componentRect.x + 130,
+                width = 160
+            };
+            var name = property.FindPropertyRelative("name");
+            name.stringValue = EditorGUI.TextField(nameRect, "", name.stringValue);
+        }
+    }
+}
+
+[CustomEditor(typeof(UIRefCollector))]
+public class UIRefCollectorInspector : Editor {
+    private UIRefCollector owner;
+    private ReorderableList reorderableList;
+
+    private void OnEnable() {
+        owner = target as UIRefCollector;
+
+        var prop = serializedObject.FindProperty("bindList");
+        reorderableList = new ReorderableList(serializedObject, prop);
+        reorderableList.elementHeight = 20;
+        reorderableList.drawElementCallback = (rect, index, active, focused) => {
+            var element = prop.GetArrayElementAtIndex(index);
+
+            Rect itemRect = new Rect(rect) {
+                x = rect.x,
+                width = 20
+            };
+            EditorGUI.LabelField(itemRect, string.Format("[{0}]", index.ToString()));
+
+            rect.height -= 4;
+            rect.y += 2;
+            EditorGUI.PropertyField(rect, element);
+        };
+
+        reorderableList.onSelectCallback += rlist => { GUI.backgroundColor = Color.blue; };
+
+        reorderableList.drawHeaderCallback = rect => { EditorGUI.LabelField(rect, prop.displayName); };
+    }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+
+        reorderableList.DoLayoutList();
+
+        if (GUILayout.Button("Check")) {
+            owner.Check();
+        }
+
+        if (GUILayout.Button("Copy")) {
+            string code = owner.Copy();
+            GUIUtility.systemCopyBuffer = code;
+            Debug.LogError(code);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
+
+public class UIRefCollector : MonoBehaviour {
+    [Serializable]
+    public class BindItem {
+        public string name;
+        public Component component;
+#if UNITY_EDITOR
+        public string componentType {
+            get {
+                if (component != null) {
+                    return component.GetType().ToString();
+                }
+
+                return null;
+            }
+        }
+
+        public string GetComponentPath(Component end) {
+            return GetPath(component, end);
+        }
+#endif
+    }
+
+    public List<BindItem> bindList = new List<BindItem>();
+
+#if UNITY_EDITOR
+    public static readonly string TAB = "    ";
+
+    public string Copy() {
+        if (!Check()) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("public GameObject gameObject { get; private set; } = null;");
+        sb.AppendLine("public Transform transform { get; private set; } = null;");
+        sb.AppendLine();
+
+        for (int i = 0, length = bindList.Count; i < length; ++i) {
+            var item = bindList[i];
+            sb.AppendFormat("// [{0}] Path: {1}", i.ToString(), item.GetComponentPath(this));
+            sb.AppendLine();
+            sb.AppendFormat("public {0} {1}", item.componentType, item.name);
+            sb.AppendLine(" { get; private set; } = null;");
+        }
+
+        sb.AppendLine();
+        sb.AppendLine("public void Bind(Transform transform) {");
+
+        sb.Append(TAB);
+        sb.AppendLine("this.transform = transform;");
+        sb.Append(TAB);
+        sb.AppendLine(@"this.gameObject = transform.gameObject;
+}");
+
+        sb.AppendLine();
+        sb.AppendLine("// 后续想不热更prefab,只通过代码查找组件的时候，写另外一个partial Find即可");
+        sb.AppendLine("public partial void Find() {");
+        sb.Append(TAB);
+        sb.AppendLine("var refCollector = transform.GetComponent<UIRefCollector>();");
+        sb.Append(TAB);
+        sb.AppendLine(@"if (refCollector == null) {
+        return;
+    }");
+
+        sb.AppendLine();
+        for (int i = 0, length = bindList.Count; i < length; ++i) {
+            var item = bindList[i];
+            sb.Append(TAB);
+            sb.AppendFormat("this.{0} = refCollector.GetComponent<{1}>({2});", item.name, item.componentType, i.ToString());
+            sb.AppendLine();
+        }
+
+        sb.AppendLine("}");
+        sb.AppendLine();
+
+        sb.AppendLine(@"public interface IListener {
+    // void OnBtnExitClicked();
+}");
+        sb.AppendLine();
+
+        sb.AppendLine(@"public void Listen(IListener listener, bool toListen = true) { 
+    // this.btnExit.onClick.AddListener(listener.OnBtnExitClicked);
+}");
+
+        return sb.ToString();
+    }
+
+    public bool Check() {
+        bool rlt = true;
+        HashSet<string> hashset = new HashSet<string>();
+        for (int i = 0, length = bindList.Count; i < length; ++i) {
+            var name = bindList[i].name;
+            if (string.IsNullOrEmpty(name)) {
+                Debug.LogErrorFormat("index: {0} has empty name", i.ToString());
+                rlt = false;
+            }
+            else {
+                name = name.Trim();
+                // https://stackoverflow.com/questions/6372318/c-sharp-string-starts-with-a-number-regex
+                if (Regex.IsMatch(name, @"^\d")) {
+                    Debug.LogErrorFormat("index: {0} start with number", i.ToString());
+                }
+                else {
+                    var component = bindList[i].component;
+                    if (component == null) {
+                        Debug.LogErrorFormat("index: {0} component is null", i.ToString());
+                        rlt = false;
+                    }
+                    else {
+                        if (!hashset.Contains(name)) {
+                            hashset.Add(name);
+                        }
+                        else {
+                            Debug.LogErrorFormat("index: {0} has same name with alreaady item", i.ToString());
+                            rlt = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return rlt;
+    }
+#endif
+
+    public T GetComponent<T>(int index) where T : Component {
+        if (index < 0 || index >= bindList.Count) {
+            Debug.LogError("Index is out of range");
+            return null;
+        }
+
+        T bindCom = bindList[index].component as T;
+        if (bindCom == null) {
+            Debug.LogErrorFormat("Index: {0} has invalid component", index.ToString());
+            return null;
+        }
+
+        return bindCom;
+    }
+
+    public static string GetPath(Component component, Component end = null) {
+        if (component == null) {
+            return null;
+        }
+
+        string totalPath = null;
+        List<string> paths = new List<string>();
+        if (end == null) {
+            var cp = component;
+            while (cp.transform.parent != null) {
+                paths.Add(cp.transform.name);
+                cp = cp.transform.parent;
+            }
+        }
+        else {
+            var cp = component;
+            while (cp.gameObject != end.gameObject) {
+                paths.Add(cp.transform.name);
+                cp = cp.transform.parent;
+            }
+        }
+
+        if (paths.Count > 1) {
+            paths.Reverse();
+            totalPath = string.Join("/", paths);
+        }
+        else if (paths.Count > 0) {
+            totalPath = paths[0];
+        }
+
+        return totalPath;
+    }
+}
