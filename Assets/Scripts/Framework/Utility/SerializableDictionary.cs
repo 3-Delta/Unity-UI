5738965a0f9ef2adb1621d21a468ee97b21e8533
cignal <cignal@users.noreply.github.com>
2022-02-20 21:26:31 +0800

    放弃 字典的序列化

---
+++ Assets/Scripts/Framework/Utility/SerializableDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 参考URP的SerializedDictionary的设计
+// 以及 https://assetstore.unity.com/packages/tools/integration/serializabledictionary-90477
+// 放弃
+[Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver {
+    [Serializable]
+    public class KVP {
+        public TKey key;
+        public TValue value;
+    }
+
+    [SerializeField] private List<KVP> list = new List<KVP>();
+
+    public void OnBeforeSerialize() { }
+
+    public void OnAfterDeserialize() { }
+}
