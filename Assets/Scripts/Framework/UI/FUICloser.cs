cadf0cdcd2cacd22992eb2973eaccd8d5f115fff
cuibinbin <18729485430@139.com>
2022-02-22 20:37:31 +0800

    提交

--- Assets/Scripts/Framework/UI/FUICloser.cs
+++ Assets/Scripts/Framework/UI/FUICloser.cs
@@ -1,19 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 [DisallowMultipleComponent]
-public class FUICloser : MonoBehaviour
-{
+public class FUICloser : MonoBehaviour {
     public int uiType;
     public Button btnExit;
     
+    public Action onClose;
+
     private void Awake() {
         if (btnExit != null) {
             btnExit.onClick.AddListener(Close);
         }
     }
-    
+
     private void Close() {
+        onClose?.Invoke();
         FUIMgr.Close(uiType);
     }
 }
