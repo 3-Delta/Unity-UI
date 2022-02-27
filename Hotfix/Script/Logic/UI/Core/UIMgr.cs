99a41e8781e4534144c25adac61f0fed3065220d
cignal <cignal@users.noreply.github.com>
2022-02-27 22:09:48 +0800

    修改代码

--- Hotfix/Script/Logic/UI/Core/UIMgr.cs
+++ Hotfix/Script/Logic/UI/Core/UIMgr.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 
 public class UIMgr : FUIMgr {
-    public static void Init() {
+    public new static void Init() {
         UIEntryRegistry.Inject();
         FUIMgr.Init();
     }
 
     public static void Open(EUIType uiType, Tuple<ulong, ulong, ulong, object> arg = null) {
-        FUIMgr.Open((int)(uiType), arg);
+        Open((int)(uiType), arg);
     }
 
     public static void Close(EUIType uiType) {
-        FUIMgr.Close((int)(uiType));
+        Close((int)(uiType));
     }
 
     public static void Show(EUIType uiType) {
-        FUIMgr.Show((int)(uiType));
+        Show((int)(uiType));
     }
 
     public static void Hide(EUIType uiType) {
-        FUIMgr.Hide((int)(uiType));
+        Hide((int)(uiType));
     }
 
     public static void CloseUtil(EUIType uiType, bool onlyJudgeOwnerStack) {
-        FUIMgr.CloseUtil((int)(uiType), onlyJudgeOwnerStack);
+        CloseUtil((int)(uiType), onlyJudgeOwnerStack);
     }
 }
