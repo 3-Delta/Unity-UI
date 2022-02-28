a03b37b43916374819306eb2134df79780cabc4c
cignal <cignal@users.noreply.github.com>
2022-03-01 01:38:43 +0800

    提交

--- Assets/Scripts/Framework/Components/StartDestroy.cs
+++ Assets/Scripts/Framework/Components/StartDestroy.cs
@@ -3,19 +3,10 @@ using UnityEngine;
 
 [DisallowMultipleComponent]
 public class StartDestroy : MonoBehaviour {
-    public bool awakeStart = false;
     public Action<bool> onTrigger;
 
-    private void Awake() {
-        if (awakeStart) {
-            onTrigger?.Invoke(true);
-        }
-    }
-
     private void Start() {
-        if (!awakeStart) {
-            onTrigger?.Invoke(true);
-        }
+        onTrigger?.Invoke(true);
     }
 
     private void OnDestroy() {
