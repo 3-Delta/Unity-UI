467b747c642d1490789c9b919879b4c8765a6803
cignal <cignal@users.noreply.github.com>
2022-03-04 01:29:05 +0800

    // 可以参考Rider的实现 // https://answer.uwa4d.com/question/5cc1190f5dad710abee80429#5cc13cfd5dad710abee80431 // https://docs.microsoft.com/zh-cn/visualstudio/cross-platform/customize-project-files-created-by-vstu?view=vs-2019 // 如何在工程变化的时候，插入热更独立工程的csproject

--- Assets/Scripts/Editor/Compiler/CompileCoprocessor.cs
+++ Assets/Scripts/Editor/Compiler/CompileCoprocessor.cs
@@ -9,7 +9,7 @@ using SyntaxTree.VisualStudio.Unity.Bridge;
 
 // https://docs.microsoft.com/zh-cn/visualstudio/cross-platform/customize-project-files-created-by-vstu?view=vs-2019
 [InitializeOnLoad]
-public class BuildCoprocessor : AssetPostprocessor {
+public class BuildCoprocessor {
     // necessary for XLinq to save the xml project file in utf8
     public class Utf8StringWriter : StringWriter {
         public override Encoding Encoding => Encoding.UTF8;
@@ -19,23 +19,12 @@ public class BuildCoprocessor : AssetPostprocessor {
         ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) => {
             Debug.LogError(name + " ===============>>>  " + content);
 
-            // parse the document and make some changes
             var document = XDocument.Parse(content);
-
-            // save the changes using the Utf8StringWriter
             var str = new Utf8StringWriter();
             document.Save(str);
 
             return str.ToString();
         };
-    }
-
-    public static string OnGeneratedSlnSolution(string path, string content) {
-        return content;
-    }
-
-    public static string OnGeneratedCSProject(string path, string content) {
-        return content;
     }
 }
 #endif
--- Assets/Scripts/Editor/Compiler/ScriptAssetPostprocessor.cs
+++ Assets/Scripts/Editor/Compiler/ScriptAssetPostprocessor.cs
@@ -1,23 +1,47 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
+
 using UnityEditor;
+
 using UnityEngine;
 
 // 可以参考Rider的实现
 // https://answer.uwa4d.com/question/5cc1190f5dad710abee80429#5cc13cfd5dad710abee80431
 // https://docs.microsoft.com/zh-cn/visualstudio/cross-platform/customize-project-files-created-by-vstu?view=vs-2019
 // 如何在工程变化的时候，插入热更独立工程的csproject
-public class ScriptAssetPostprocessor : AssetPostprocessor {
+public class ScriptAssetPostprocessor : AssetPostprocessor
+{
+    // 编译成果回调
     [UnityEditor.Callbacks.DidReloadScripts]
     private static void OnCompiled()
     {
         Debug.LogError("编译成功");
     }
-    
-    public static string OnGeneratedSlnSolution(string path, string content) {
-        return content;
+
+    // 参考：
+    // 设置启动项目 https://www.cnblogs.com/dudu/p/set-default-startup-project-in-visual-studio.html
+    // https://qiita.com/toRisouP/items/6b0fc5eb97b0d7ce1499
+    // 7. https://blog.csdn.net/u010019717/article/details/89737747
+    public static string OnGeneratedSlnSolution(string path, string content)
+    {
+        string slnGUID = "FAE04EC0-301F-11D3-BF4B-00C04F79EFBC";
+        string prjGUID = "9AA23530-C9A6-478D-90A4-C897C7147769";
+        string prjName = "Logic.Hotfix";
+        string prjPath = @"Hotfix\Logic.Hotfix.csproj";
+
+        if (content.Contains(prjName))
+        {
+            return content;
+        }
+
+        string append = $"Project(\"{slnGUID}\") = \"{prjName}\", \"{ prjPath}\", \"{prjGUID}\", \"{Environment.NewLine}\"EndProject";
+        string newContent = content.Replace($"EndProject{Environment.NewLine}Global",
+            $"EndProject{Environment.NewLine}{append}{Environment.NewLine}Global");
+        return newContent;
     }
 
-    public static string OnGeneratedCSProject(string path, string content) {
+    public static string OnGeneratedCSProject(string path, string content)
+    {
         return content;
     }
 }
