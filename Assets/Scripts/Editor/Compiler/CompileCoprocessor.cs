b11824ef891c140cecb5d4be1b74aeab91abf67f
cignal <cignal@users.noreply.github.com>
2022-03-04 01:32:15 +0800

    ENABLE_VSTU 会在使用vs打开的时候才生效

--- Assets/Scripts/Editor/Compiler/CompileCoprocessor.cs
+++ Assets/Scripts/Editor/Compiler/CompileCoprocessor.cs
@@ -4,19 +4,24 @@ using System.Xml.Linq;
 using UnityEngine;
 using UnityEditor;
 
+// 使用vs打开，该宏才会生效
 #if ENABLE_VSTU
 using SyntaxTree.VisualStudio.Unity.Bridge;
 
 // https://docs.microsoft.com/zh-cn/visualstudio/cross-platform/customize-project-files-created-by-vstu?view=vs-2019
 [InitializeOnLoad]
-public class BuildCoprocessor {
+public class BuildCoprocessor
+{
     // necessary for XLinq to save the xml project file in utf8
-    public class Utf8StringWriter : StringWriter {
+    public class Utf8StringWriter : StringWriter
+    {
         public override Encoding Encoding => Encoding.UTF8;
     }
 
-    static BuildCoprocessor() {
-        ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) => {
+    static BuildCoprocessor()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) =>
+        {
             Debug.LogError(name + " ===============>>>  " + content);
 
             var document = XDocument.Parse(content);
@@ -24,6 +29,11 @@ public class BuildCoprocessor {
             document.Save(str);
 
             return str.ToString();
+        };
+
+        ProjectFilesGenerator.SolutionFileGeneration += (string fileName, string fileContent) =>
+        {
+            return fileContent;
         };
     }
 }
