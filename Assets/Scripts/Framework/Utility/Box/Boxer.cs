5bfc10938692c0f14970c74e5b9eb525828f7749
cignal <cignal@users.noreply.github.com>
2022-04-21 16:57:22 +0800

    https://zhuanlan.zhihu.com/p/476725200
    huatuo qq群：优化装箱：装箱这个问题我们的解决方案是重用装箱，提前装箱一个对象，然后直接指针操纵里面的数据，就可以重用了，无GC装箱

---
+++ Assets/Scripts/Framework/Utility/Box/Boxer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+// https://zhuanlan.zhihu.com/p/476725200
+// 以及clr via c# page.111
+public class ClassHeader {
+    public byte value = 0;
+
+    public static int HeaderSize {
+        get { return UnsafeUtility.GetFieldOffset(typeof(ClassHeader).GetField(nameof(value))); }
+    }
+}
+
+// int等内置值类型也可以通过这个struct的限制
+public static class Boxer<T> where T : struct {
+    // 提前box创建一个模板
+    private static readonly object _BOXER = Activator.CreateInstance<T>();
+
+    public unsafe static object Box(ref T target) {
+        byte* ptr = (byte*)UnsafeUtility.PinGCObjectAndGetAddress(_BOXER, out ulong gcHandler);
+        byte* valuePtr = ptr + ClassHeader.HeaderSize;
+        // 高C#版本可以直接：ref T t = UnsafeUtility.AsRef<T>((void*)ptr);
+        UnsafeUtility.CopyStructureToPtr(ref target, valuePtr);
+        UnsafeUtility.ReleaseGCObject(gcHandler);
+        return _BOXER;
+    }
+}
