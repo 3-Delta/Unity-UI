96f3fddd059ed0ba1b2f0cd56818a7782e00559f
cignal <cignal@users.noreply.github.com>
2022-04-28 21:33:37 +0800

    MondrianBlock

---
+++ Assets/MondrianBlock/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ESelectType {
+    Red,
+    Normal
+}
+
+public class Block : MonoBehaviour {
+    public int x;
+    public int y;
+    public bool used = false;
+    public BlockSetter setter;
+
+    private void Awake() {
+        this.GetComponent<Button>().onClick.AddListener(OnBtnClicked);
+    }
+
+    public void SetSelect(bool toSelect, ESelectType selectType, Color color) {
+        var img = this.GetComponent<Image>();
+        if (selectType == ESelectType.Red) {
+            img.color = Color.red;
+            used = true;
+        }
+        else {
+            used = toSelect;
+            img.color = toSelect ? color : Color.white;
+        }
+    }
+
+    private void OnBtnClicked() {
+        if (this.setter.status == EPlayTStatus.Set) {
+            SetSelect(true, ESelectType.Red, Color.white);
+        }
+    }
+}
---
+++ Assets/MondrianBlock/BlockSetter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum EPlayTStatus {
+    Reset,
+    Set,
+    Play,
+}
+
+public class BlockSetter : MonoBehaviour {
+    public EPlayTStatus status = EPlayTStatus.Set;
+    public Button btnReset;
+    public Button btnSet;
+    public Button btnStart;
+
+    public MondrianBlocks mondrianBlocks;
+
+    private void Awake() {
+        this.btnReset.onClick.AddListener(OnBtnClickedReset);
+        this.btnSet.onClick.AddListener(OnBtnClickedSet);
+        this.btnStart.onClick.AddListener(OnBtnClickedStart);
+
+        status = EPlayTStatus.Set;
+    }
+
+    private void OnBtnClickedReset() {
+        foreach (var block in mondrianBlocks.blocks) {
+            block.SetSelect(false, ESelectType.Normal, Color.white);
+        }
+
+        foreach (var rct in this.mondrianBlocks.rcts) {
+            rct.used = false;
+        }
+
+        status = EPlayTStatus.Reset;
+    }
+
+    private void OnBtnClickedSet() {
+        if (status == EPlayTStatus.Reset) {
+            this.status = EPlayTStatus.Set;
+        }
+    }
+
+    private void OnBtnClickedStart() {
+        if (status == EPlayTStatus.Set) {
+            this.status = EPlayTStatus.Play;
+
+            mondrianBlocks.Find(0, 0);
+        }
+    }
+}
