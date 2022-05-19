dc5b5e1544cff5fd06b2f9c4e4eed5f1105e2bea
cignal <cignal@users.noreply.github.com>
2022-05-19 21:26:03 +0800

    红点系统定位器

---
+++ Assets/Scripts/Framework/UI/RedDot/RedDotCollector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ERedDotType {
+    Pure,
+    WithNumber,
+    New,
+}
+
+// 每个UI根节点挂载的红点的配置
+// 如果是动态生成的节点上需要红点，则使用proto的配置
+[DisallowMultipleComponent]
+public class RedDotCollector : MonoBehaviour {
+    public List<RedDotIndexer> items = new List<RedDotIndexer>(2);
+
+#region 动态注册
+    public void Register(in RedDotIndexer indexer) {
+        if (!this.items.Contains(indexer)) {
+            this.items.Add(indexer);
+        }
+    }
+
+    public void UnRegister(in RedDotIndexer indexer) {
+        this.items.Remove(indexer);
+    }
+#endregion
+}
---
+++ Assets/Scripts/Framework/UI/RedDot/RedDotIndexer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ILRuntime.CLR.Utils;
+using UnityEngine;
+
+// 在红点需要出现的节点上挂载
+[DisallowMultipleComponent]
+public class RedDotIndexer : MonoBehaviour {
+    public uint id;
+    // public Func<bool> CanShow;
+    // public Action<RedDotIndexer> ShowAction;
+    //
+    // public void Set() {
+    //     
+    // }
+    //
+    // private void TryLoad() {
+    //     
+    // }
+    //
+    // public void Listen(bool toListen) {
+    //     
+    // }
+
+    private RedDotCollector _collector;
+
+    public RedDotCollector Collector {
+        get {
+            if (this._collector == null) {
+                this._collector = this.GetComponentInParent<RedDotCollector>();
+            }
+
+            return this._collector;
+        }
+    }
+
+    private void OnEnable() {
+        if (this.Collector != null) {
+            this.Collector.Register(this);
+        }
+    }
+
+    private void OnDisable() {
+        if (this.Collector != null) {
+            this.Collector.UnRegister(this);
+        }
+    }
+}
