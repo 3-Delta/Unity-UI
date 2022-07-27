d96b3423f8501713597f9cfbdbaad36560faeed1
kaclok <572005238@qq.com>
2022-07-27 18:35:25 +0800

    TabPage
    // 用于处理福利活动这种点击切页然后不同的表现page的显示刷新
    // tablist需要支持无限滚动，悬停，动态增删的校验
    // page可以服用
    // tab和page的对应关系可以提前构建。

---
+++ Assets/Scripts/Framework/Components/Selectable/TabPage/TabPage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 用于处理福利活动这种点击切页然后不同的表现page的显示刷新
+// tablist需要支持无限滚动，悬停，动态增删的校验
+// page可以服用
+// tab和page的对应关系可以提前构建。
+[DisallowMultipleComponent]
+public class TabPage : MonoBehaviour {
+}
