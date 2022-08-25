67689335dd19580c1c9c13c0971d2cf5b7c0fb70
kaclok <572005238@qq.com>
2022-08-25 20:01:33 +0800

    添加强制奔溃的测试接口

--- Assets/Scripts/Framework/Services/DeviceService.cs
+++ Assets/Scripts/Framework/Services/DeviceService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.Diagnostics;
 
 // https://answer.uwa4d.com/question/62f5cfc8343a54268c841b28
 // app所在的设备的信息
-public static class DeviceService
-{
+public static class DeviceService {
     // 当前机器是64bit吗
     public static bool Is64bit => IntPtr.Size == 64;
 
     public static bool Is32bit => IntPtr.Size == 32;
+
+    public static void ForceCrash() {
+        // 强制奔溃，方柏霓一些奔溃测试
+        Utils.ForceCrash(ForcedCrashCategory.AccessViolation);
+    }
 }
