5a2eaf5eedd981859601c2c65414e8ee299b816c
cignal <cignal@users.noreply.github.com>
2022-09-05 01:32:02 +0800

    参考ref

--- Assets/Scripts/Framework/Actor/Controller/Sync/LocalInputReceiver.cs
+++ Assets/Scripts/Framework/Actor/Controller/Sync/LocalInputReceiver.cs
@@ -3,6 +3,7 @@
 // 接受键鼠/遥感的输入
 // 将来从某个统一的地方接收输入，便于统一管理输入的开启,关闭
 // 监听InputMgr的各种action
+// https://code.aliyun.com/kaclok/UnityPhysics.git
 [DisallowMultipleComponent]
 public class LocalInputReceiver : MonoBehaviour {
     // 后续细分各种操作，比如遥感，键盘，鼠标
