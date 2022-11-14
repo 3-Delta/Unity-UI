f36e721f8b44cb59ab124bc92ab6ed278e9d0bd2
kaclok <572005238@qq.com>
2022-11-14 17:56:24 +0800

    cow添加可以手动插入go的方式，而不是全是自动化插入

--- Assets/Scripts/Framework/Utility/COW/COWComponentQuickLoader.cs
+++ Assets/Scripts/Framework/Utility/COW/COWComponentQuickLoader.cs
@@ -40,7 +40,7 @@ public class COWLoader<T> where T : Component {
         this.parent = parent;
     }
 
-    private COWLoader<T> TryBuild(int targetCount, Action<ComponentCell<T>, int /* index */> onInit) {
+    public COWLoader<T> TryBuildGo(int targetCount, Action<ComponentCell<T>, int /* index */> onInit) {
         proto.SetActive(false);
         while (targetCount > this.Count) {
             Transform clone = GameObject.Instantiate<GameObject>(proto.gameObject, parent).transform;
@@ -49,19 +49,25 @@ public class COWLoader<T> where T : Component {
             clone.localScale = Vector3.one;
             clone.gameObject.SetActive(false);
 
-            if (!clone.TryGetComponent<ComponentCell<T>>(out var rlt)) {
-                rlt = clone.gameObject.AddComponent<ComponentCell<T>>();
-            }
-
-            onInit?.Invoke(rlt, this.Count);
-
-            this._components.Add(rlt);
+            TryAddGo(clone, onInit);
         }
 
         return this;
     }
 
-    private COWLoader<T> TryRefresh(int targetCount, Action<ComponentCell<T>, int /* index */> onRefresh) {
+    // 为了方便将一些特定的go添加进入，而不是统一样式的go
+    public COWLoader<T> TryAddGo(Transform clone, Action<ComponentCell<T>, int /* index */> onInit) {
+        if (!clone.TryGetComponent<ComponentCell<T>>(out var rlt)) {
+            rlt = clone.gameObject.AddComponent<ComponentCell<T>>();
+        }
+
+        onInit?.Invoke(rlt, this.Count);
+
+        this._components.Add(rlt);
+        return this;
+    }
+
+    public COWLoader<T> TryRefresh(int targetCount, Action<ComponentCell<T>, int /* index */> onRefresh) {
         this.RealCount = targetCount;
         int componentCount = this.Count;
         for (int i = 0; i < componentCount; ++i) {
@@ -79,7 +85,57 @@ public class COWLoader<T> where T : Component {
     }
 
     public COWLoader<T> TryBuildOrRefresh(int targetCount, Action<ComponentCell<T>, int /* index */> onInit, Action<ComponentCell<T>, int /* index */> onRefresh) {
-        this.TryBuild(targetCount, onInit);
+        this.TryBuildGo(targetCount, onInit);
         return this.TryRefresh(targetCount, onRefresh);
     }
 }
+
+// 数据层面的复用
+public class COW<T> {
+    private readonly List<T> ls = new List<T>();
+    
+    public int Count {
+        get { return this.ls.Count; }
+    }
+
+    public int RealCount { get; private set; }
+
+    public T this[int index] {
+        get { return ls[index]; }
+    }
+
+    public void Clear() {
+        ls.Clear();
+        RealCount = 0;
+    }
+
+    public COW<T> TrySet<P>(int targetCount, IList<P> list, Func<int /*index*/, P /*data*/, T> onCreate) {
+        RealCount = targetCount;
+        while (targetCount > Count) {
+            int index = Count;
+            T t = onCreate.Invoke(index, list[index]);
+            
+            this.ls.Add(t);
+        }
+        return this;
+    }
+    
+    public COW<T> TrySet<TKey, TValue>(int targetCount, IDictionary<TKey, TValue> dict, Func<int /*index*/, IDictionary<TKey, TValue> /*data*/, T> onCreate) {
+        RealCount = targetCount;
+        while (targetCount > Count) {
+            int index = Count;
+            T t = onCreate.Invoke(index, dict);
+            
+            this.ls.Add(t);
+        }
+        return this;
+    }
+
+    public void Add(T t, bool activeRealCount) {
+        this.ls.Add(t);
+        if (activeRealCount) {
+            RealCount += 1;
+        }
+    }
+}
+
