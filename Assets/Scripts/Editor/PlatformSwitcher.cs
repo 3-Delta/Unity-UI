82a2a5fecad793e7763d1ea8e073cedfcd6253f7
kaclok <572005238@qq.com>
2022-12-06 15:36:41 +0800

    切换平台的时候， 将某些特定平台的文件或者文件夹隐藏/显示 比如ios平台下的一些代码，如果切换到pc平台就会报错，因为文件众多，不想逐个文件打开添加marco规避。
    注意 不要使用assetdatabase处理。

---
+++ Assets/Scripts/Editor/PlatformSwitcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build;
+
+// 要求文件夹名字和平台名字对应
+public class PlatformSwitcher : IActiveBuildTargetChanged {
+    [System.Serializable]
+    public class Item {
+        public string path;
+        public bool meta;
+    }
+
+    public readonly List<Item> Paths = new List<Item>() {
+        new Item() {
+            path = $"{Application.dataPath}/../OffLinePackages/Platform",
+            meta = false 
+        },
+    };
+
+    public int callbackOrder { get; }
+
+    public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget) {
+        Debug.LogError("OnActiveBuildTargetChanged:" + previousTarget + " -> " + newTarget);
+
+        if (previousTarget == newTarget) {
+            return;
+        }
+
+        // https://docs.unity3d.com/Manual/SpecialFolders.html
+        // 文件或者文件夹以.开头 或者 以~结尾，都会被unity忽略，也就是说不会生成meta文件，也就意味着AssetDataBase系列API不能使用
+        foreach (var one in Paths) {
+            string oldPath = $"{one.path}/{previousTarget}";
+            string newPath = $"{one.path}/.{previousTarget}";
+            if (Directory.Exists(oldPath)) {
+                Directory.Move(oldPath, newPath);
+                if(one.meta) {
+                    File.Move(oldPath + ".meta", newPath + ".meta");
+                }
+            }
+
+            oldPath = $"{one.path}/.{newTarget}";
+            newPath = $"{one.path}/{newTarget}";
+            if (Directory.Exists(oldPath)) {
+                Directory.Move(oldPath, newPath);
+                if(one.meta) {
+                    File.Move(oldPath + ".meta", newPath + ".meta");
+                }
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
