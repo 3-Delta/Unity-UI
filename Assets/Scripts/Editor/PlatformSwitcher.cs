99a1f8d996a48a3ef5a6659b778fbb33baf98562
kaclok <572005238@qq.com>
2022-12-06 15:36:41 +0800

    切换平台的时候， 将某些特定平台的文件或者文件夹隐藏/显示 比如ios平台下的一些代码，如果切换到pc平台就会报错，因为文件众多，不想逐个文件打开添加marco规避。
    注意 不要使用assetdatabase处理。

---
+++ Assets/Scripts/Editor/PlatformSwitcher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build;
+
+public class PlatformSwitcher : IActiveBuildTargetChanged {
+    public readonly List<BuildTarget> PLATFORMS = new List<BuildTarget>() {
+        BuildTarget.StandaloneWindows64,
+        BuildTarget.Android,
+        BuildTarget.iOS,
+        BuildTarget.StandaloneOSX,
+    };
+
+    public string Folder = "Platform";
+
+    public void Reset() {
+        BuildTarget current = EditorUserBuildSettings.activeBuildTarget;
+    }
+
+    public int callbackOrder { get; }
+
+    public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget) {
+        Debug.LogError("OnActiveBuildTargetChanged:" + previousTarget + " -> " + newTarget);
+
+        if (previousTarget == newTarget) {
+            return;
+        }
+
+        // https://docs.unity3d.com/Manual/SpecialFolders.html
+        // 文件或者文件夹以.开头 或者 以~结尾，都会被unity忽略，也就是说不会生成meta文件，也就意味着AssetDataBase系列API不能使用
+        string oldPath = $"{Application.dataPath}/{Folder}/{previousTarget}";
+        string newPath = $"{Application.dataPath}/{Folder}/.{previousTarget}";
+        if (Directory.Exists(oldPath)) {
+            Directory.Move(oldPath, newPath);
+            File.Move(oldPath + ".meta", newPath + ".meta");
+        }
+
+        oldPath = $"{Application.dataPath}/{Folder}/.{newTarget}";
+        newPath = $"{Application.dataPath}/{Folder}/{newTarget}";
+        if (Directory.Exists(oldPath)) {
+            Directory.Move(oldPath, newPath);
+            File.Move(oldPath + ".meta", newPath + ".meta");
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
