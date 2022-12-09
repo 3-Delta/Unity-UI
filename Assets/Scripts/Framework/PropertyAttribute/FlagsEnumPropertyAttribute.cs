0d94602b33a0fcd9c50845837954af8fd9adc46f
kaclok <572005238@qq.com>
2022-12-09 14:37:41 +0800

    enumflags的序列化

---
+++ Assets/Scripts/Framework/PropertyAttribute/FlagsEnumPropertyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(FlagsEnumPropertyAttribute))]
+public class FlagsEnumPropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+    }
+}
+#endif
+
+// 在枚举的field是附属，需要定义枚举为[System.Flags]
+// 即第一个枚举一定要是1，而不是0
+[AttributeUsage(AttributeTargets.Field)]
+public class FlagsEnumPropertyAttribute : PropertyAttribute {
+}
