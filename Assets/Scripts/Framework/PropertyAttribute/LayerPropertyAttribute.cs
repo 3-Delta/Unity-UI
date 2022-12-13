6de29da9409fb7120bf6bb0b5fbd935e88d529cb
kaclok <572005238@qq.com>
2022-12-13 13:23:02 +0800

    tag layer的序列化

---
+++ Assets/Scripts/Framework/PropertyAttribute/LayerPropertyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(LayerPropertyAttribute))]
+public class LayerPropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (property.propertyType != SerializedPropertyType.Integer) {
+            Debug.LogWarning("LayerPropertyAttribute can only be applied on integer properties/fields");
+            return;
+        }
+
+        property.intValue = EditorGUI.LayerField(position, property.name, property.intValue);
+    }
+}
+#endif
+
+// public int layer;
+[AttributeUsage(AttributeTargets.Field)]
+public class LayerPropertyAttribute : PropertyAttribute {
+}
---
+++ Assets/Scripts/Framework/PropertyAttribute/TagPropertyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(TagPropertyAttribute))]
+public class TagPropertyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if (property.propertyType != SerializedPropertyType.String) {
+            Debug.LogWarning("TagPropertyAttribute can only be applied on string properties/fields");
+            return;
+        }
+
+        property.stringValue = EditorGUI.TagField(position, property.name, property.stringValue);
+    }
+}
+#endif
+
+// public string tag;
+[AttributeUsage(AttributeTargets.Field)]
+public class TagPropertyAttribute : PropertyAttribute {
+}
