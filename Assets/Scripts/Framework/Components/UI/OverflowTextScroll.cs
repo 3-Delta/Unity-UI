77c02309c6c796a83936e95f3f09ebf473b46a23
cignal <cignal@users.noreply.github.com>
2023-01-08 17:27:25 +0800

    https://zhuanlan.zhihu.com/p/596800428 UGUI 文字移动跑马灯效果
    localPosition与anchoredPosition转化 https://zhuanlan.zhihu.com/p/114829066
    https://www.jianshu.com/p/dbefa746e50d

---
+++ Assets/Scripts/Framework/Components/UI/OverflowTextScroll.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ref: https://copyfuture.com/blogs-details/202211130534096229
+// https://zhuanlan.zhihu.com/p/596800428
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Text), typeof(ContentSizeFitter))]
+public class OverflowTextScroll : MonoBehaviour {
+    public enum EScrollMode {
+        R2L, // 从右到左
+        L2R, // 从左到右
+
+        R2LPingPong, // 从右到左循环
+        L2RPingPong, // 从左到右循环
+    }
+
+    public EScrollMode scrollMode = EScrollMode.R2L;
+    public float scrollSpeed = 80f;
+    public bool useRealTime = false;
+    // 如果显示区域可以显示全，是否强制scroll
+    public bool forceScroll = false;
+
+    // parent节点anchor和pivot都是0.5
+    private RectTransform parentRect;
+    private RectTransform selfRect;
+    private Text text;
+
+    // 超框的时候激活
+    private bool canMove = false;
+    private bool isReverse = false;
+
+    private Vector2 curAnchorPos;
+    private float leftEdge;
+    private float rightEdge;
+
+    private void Awake() {
+        text = this.GetComponent<Text>();
+        selfRect = this.GetComponent<RectTransform>();
+        parentRect = this.transform.parent.GetComponent<RectTransform>();
+    }
+
+    private IEnumerator Start() {
+        // ContentSizeFitter需要等一帧才会得到生效后的数据
+        // ContentSizeFitter主要是收缩Text的渲染size，cachedTextGeneratorForLayout和rect保持一致
+        yield return null;
+        this.canMove = this.Judge();
+        this.canMove |= this.forceScroll;
+    }
+
+    private void Update() {
+        if (this.canMove) {
+            var d = this.scrollSpeed * (this.useRealTime ? Time.unscaledDeltaTime : Time.deltaTime);
+            curAnchorPos = this.selfRect.localPosition;
+
+            if (scrollMode == EScrollMode.R2L) {
+                curAnchorPos.x -= d;
+                if (curAnchorPos.x < this.leftEdge) {
+                    curAnchorPos.x = this.rightEdge;
+                }
+            }
+            else if (scrollMode == EScrollMode.L2R) {
+                curAnchorPos.x += d;
+                if (curAnchorPos.x > this.rightEdge) {
+                    curAnchorPos.x = this.leftEdge;
+                }
+            }
+            else if (scrollMode == EScrollMode.R2LPingPong) {
+                if (!this.isReverse) {
+                    curAnchorPos.x -= d;
+                    if (curAnchorPos.x < this.leftEdge) {
+                        this.isReverse = !this.isReverse;
+                    }
+                }
+                else {
+                    curAnchorPos.x += d;
+                    if (curAnchorPos.x > this.rightEdge) {
+                        this.isReverse = !this.isReverse;
+                    }
+                }
+            }
+            else if (scrollMode == EScrollMode.L2RPingPong) {
+                if (this.isReverse) {
+                    curAnchorPos.x -= d;
+                    if (curAnchorPos.x < this.leftEdge) {
+                        this.isReverse = !this.isReverse;
+                    }
+                }
+                else {
+                    curAnchorPos.x += d;
+                    if (curAnchorPos.x > this.rightEdge) {
+                        this.isReverse = !this.isReverse;
+                    }
+                }
+            }
+
+            this.selfRect.localPosition = this.curAnchorPos;
+        }
+    }
+
+    // 给Text重新赋值之后
+    [ContextMenu(nameof(Begin))]
+    public void Begin() {
+        this.StopCoroutine(nameof(Start));
+        this.StartCoroutine(nameof(Start));
+    }
+
+    // 计算左右边界
+    private bool Judge() {
+        var displayWidth = this.parentRect.rect.width;
+        var textWidth = this.selfRect.rect.width;
+
+        // Text的渲染宽度大于显示区域的宽度,表示需要scroll
+        bool can = textWidth > displayWidth;
+        // 正常情况下这两句已经满足，但是如果初始情况下text不在parent的最左侧，那么这个处理就会出现问题
+        // leftX = rectTrans.anchoredPosition.x - textWidth;
+        // rightX = rectTrans.anchoredPosition.x + displayWidth;
+
+        /* 方案1：用anchorPosition的方式计算
+            // 不管Text在parent下是什么位置，一定要保证在从右往左的时候，text的最右侧在parent的最左侧，text的最左侧要在parent的最右侧。
+            var anchorX = (this.selfRect.anchorMin.x + this.selfRect.anchorMax.x) / 2;
+            this.leftEdge = -anchorX * displayWidth - (1 - selfRect.pivot.x) * textWidth;
+            this.rightEdge = (1 - anchorX) * displayWidth + selfRect.pivot.x * textWidth;
+        */
+        
+        // 方案2：用localPosition的方式计算
+        this.leftEdge = this.parentRect.localPosition.x - (displayWidth / 2f) - (1 - this.selfRect.pivot.x) * textWidth;
+        this.rightEdge = this.leftEdge + displayWidth + textWidth;
+        
+        return can;
+    }
+}
