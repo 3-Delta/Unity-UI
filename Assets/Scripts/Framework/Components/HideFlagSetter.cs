81fad31bef23b5ff734df358abf96a4ba4718a4c
cignal <cignal@users.noreply.github.com>
2023-03-29 02:01:32 +0800

    https://www.zhihu.com/pin/1624079351552380928 查看和设置某个component的hideflags的辅助工具

---
+++ Assets/Scripts/Framework/Components/HideFlagSetter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class HideFlagSetter : MonoBehaviour {
+#if UNITY_EDITOR
+    public Component target;
+    [FlagsEnumProperty] public HideFlags flags = HideFlags.None;
+
+    private void Reset() {
+        // 当前脚本不构建
+        this.hideFlags = HideFlags.DontSaveInBuild;
+    }
+
+    [ContextMenu("显示")]
+    private void Inspect() {
+        // 显示目标target的hideFlags
+        if (this.target != null) {
+            this.flags = this.target.hideFlags;
+        }
+    }
+
+    [ContextMenu("设置")]
+    private void Set() {
+        // 设置目标target的hideFlags
+        if (this.target != null) {
+            this.target.hideFlags = this.flags;
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/Framework/Services/ScreenService.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class ScreenService {
+    // 横屏/竖屏
+    public static bool PortraitOrLandscape {
+        get {
+            var cur = Screen.orientation;
+            if (cur == ScreenOrientation.Portrait || cur == ScreenOrientation.PortraitUpsideDown) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
