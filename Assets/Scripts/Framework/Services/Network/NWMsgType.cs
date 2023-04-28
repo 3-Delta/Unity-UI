68063ac7ca3d34edc72959b7b798b6af8a91aac7
cignal <cignal@users.noreply.github.com>
2023-04-28 11:32:34 +0800

    // 利用位域bitfield处理协议号，其实就是将协议号用两部分组装起来，一部分是first，也就是主模块号，另一部分是second也就是该模块内具体的协议号 // 可以将该代码分装成C++的dll，这样客户端可以使用，服务端也可以使用 // https://www.zhihu.com/pin/1633497757422940160

---
+++ Assets/Scripts/Framework/Services/Network/NWMsgType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// 利用位域bitfield处理协议号，其实就是将协议号用两部分组装起来，一部分是first，也就是主模块号，另一部分是second也就是该模块内具体的协议号
+// 可以将该代码分装成C++的dll，这样客户端可以使用，服务端也可以使用
+// https://www.zhihu.com/pin/1633497757422940160
+public class NWMsgType
+{
+    /*
+    union U {
+        uint16_t u16;
+            struct ST { 
+            uint16_t first : 10; 
+            uint16_t second : 6;
+        }st;
+    };
+
+    uint16_t GetU16(uint16_t first, uint16_t second) {
+        U u;
+        u.st.first = first;
+        u.st.second = second;
+        return u.u16;
+    }
+
+    void GetFirstSecond(uint16_t u16, uint16_t& first, uint16_t& second) {
+        U u;
+        u.u16 = u16;
+        first = u.st.first;
+        second = u.st.second;
+    }
+    */
+}
