2cb2a4ce9d967ca50ed11a1c861d67eeaf2ed974
cignal <cignal@users.noreply.github.com>
2023-05-23 00:30:44 +0800

    BoolUtils 提供anytrue, anyfalse, alltrue, allfalse的接口实现

---
+++ Assets/Scripts/Framework/Utility/BoolUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoolUtils {
+    public static bool AllTrue<T>(IList<T> container, Func<int, T, bool> condition, out int failIndex) {
+        bool ret = true;
+        failIndex = -1;
+        for (int i = 0, length = container.Count; i < length; ++i) {
+            ret &= (condition.Invoke(i, container[i]));
+            if (!ret) {
+                failIndex = i;
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    public static bool AllTrue<T>(ICollection<T> container, Func<T, bool> condition) {
+        bool ret = true;
+        foreach (var one in container) {
+            ret &= (condition.Invoke(one));
+            if (!ret) {
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    public static bool AllFalse<T>(IList<T> container, Func<int, T, bool> condition, out int failIndex) {
+        bool ret = false;
+        failIndex = -1;
+        for (int i = 0, length = container.Count; i < length; ++i) {
+            ret |= (condition.Invoke(i, container[i]));
+            if (ret) {
+                failIndex = i;
+                break;
+            }
+        }
+
+        return !ret;
+    }
+
+    public static bool AllFalse<T>(ICollection<T> container, Func<T, bool> condition) {
+        bool ret = false;
+        foreach (var one in container) {
+            ret |= (condition.Invoke(one));
+            if (ret) {
+                break;
+            }
+        }
+
+        return !ret;
+    }
+
+    public static bool AnyTrue<T>(IList<T> container, Func<int, T, bool> condition, out int firstIndex) {
+        return !AllFalse(container, condition, out firstIndex);
+    }
+
+    public static bool AnyTrue<T>(ICollection<T> container, Func<T, bool> condition) {
+        return !AllFalse(container, condition);
+    }
+
+    public static bool AnyFalse<T>(IList<T> container, Func<int, T, bool> condition, out int firstIndex) {
+        return !AllTrue(container, condition, out firstIndex);
+    }
+
+    public static bool AnyFalse<T>(ICollection<T> container, Func<T, bool> condition) {
+        return !AllTrue(container, condition);
+    }
+}
