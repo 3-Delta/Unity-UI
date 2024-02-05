d8bab17e19d18fb55ea506dfcc466b3eeefa4fad
cignal <cignal@users.noreply.github.com>
2024-02-05 21:21:38 +0800

    时间辅助函数

---
+++ Assets/Scripts/Framework/Utility/TimeUtility.cs
@@ -0,0 +1,26 @@
+﻿public static class TimeUtility
+{
+    // 获取hour
+    public static int GetH(int second)
+    {
+        return second / 3600;
+    }
+
+    // 获取minutes
+    public static int GetM(int second)
+    {
+        return second / 60 % 60;
+    }
+
+    // 获取seconds
+    public static int GetS(int second)
+    {
+        return second % 3600;
+    }
+
+    // 获取day
+    public static int GetD(int second)
+    {
+        return second / 3600 / 24;
+    }
+}
