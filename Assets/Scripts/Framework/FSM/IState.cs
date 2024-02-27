02ff63d0afc618eb4cf3d926b1fd8798066567b3
kaclok <572005238@qq.com>
2024-02-27 11:57:47 +0800

    Update IState.cs

--- Assets/Scripts/Framework/FSM/IState.cs
+++ Assets/Scripts/Framework/FSM/IState.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 // https://study.163.com/course/courseLearn.htm?courseId=1003000008#/learn/video?lessonId=1003441078&courseId=1003000008
 namespace FSM {
     public interface IState : IName {
-        // ËùÊô×´Ì¬»ú
+        // æ‰€å±çŠ¶æ€æœº
         IStateMachine fsm { get; set; }
         float time { get; }
 
-        void Enter(IState prev);
+        void Enter(IState prev); 
         void Exit(IState next);
         void Update(float deltaTime);
 
-        // ×´Ì¬×ª»»Ïà¹Ø½Ó¿Ú
+        // çŠ¶æ€è½¬æ¢ç›¸å…³æ¥å£
         List<ITransition> transitions { get; }
         void AddTransition(ITransition target);
         void RemoveTransition(ITransition target);
