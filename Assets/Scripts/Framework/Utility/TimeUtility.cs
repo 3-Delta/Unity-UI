a393329252157394aef1f932bfbd762179881c05
cignal <cignal@users.noreply.github.com>
2024-03-21 10:35:17 +0800

    时间格式化

--- Assets/Scripts/Framework/Utility/TimeUtility.cs
+++ Assets/Scripts/Framework/Utility/TimeUtility.cs
@@ -1,26 +1,21 @@
-﻿public static class TimeUtility
-{
+﻿public static class TimeUtility {
     // 获取hour
-    public static int GetH(int second)
-    {
+    public static int GetH(int second) {
         return second / 3600;
     }
 
     // 获取minutes
-    public static int GetM(int second)
-    {
+    public static int GetM(int second) {
         return second / 60 % 60;
     }
 
     // 获取seconds
-    public static int GetS(int second)
-    {
+    public static int GetS(int second) {
         return second % 3600;
     }
 
     // 获取day
-    public static int GetD(int second)
-    {
+    public static int GetD(int second) {
         return second / 3600 / 24;
     }
 }
